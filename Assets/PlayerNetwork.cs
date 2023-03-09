c54088aefb366d8b60fd1682f19fd3618028e050
Jamie Sullivan-Phillips <47346975+jsullivanphillips@users.noreply.github.com>
2023-03-08 16:00:35 -0800

    Network variables syncing. Includes example of how to send custom data and Fixed Strings

---
+++ Assets/PlayerNetwork.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using Unity.Collections;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+
+     
+    NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+       new MyCustomData{
+            _int = 56,
+            _bool = true,
+       }, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct MyCustomData : INetworkSerializable {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (MyCustomData previousValue, MyCustomData newValue) => {
+            Debug.Log(OwnerClientId + "; " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
+
+
+    void Update()
+    {
+        
+        if(!IsOwner) return;
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            randomNumber.Value = new MyCustomData {
+                _int = 10,
+                _bool = false,
+                message = "Kowabunga!",
+            };
+        }
+
+        Vector3 moveDir = new Vector3(0,0);
+        if(Input.GetKey(KeyCode.W)) moveDir.y = +1f;
+        if(Input.GetKey(KeyCode.S)) moveDir.y = -1f;
+        if(Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if(Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
