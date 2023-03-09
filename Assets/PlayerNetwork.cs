8b6742b0dff743d65fe7ad3881276e20831ecc86
Jamie Sullivan-Phillips <47346975+jsullivanphillips@users.noreply.github.com>
2023-03-08 16:49:22 -0800

    clientrpc. called on server and runs on all clients

--- Assets/PlayerNetwork.cs
+++ Assets/PlayerNetwork.cs
@@ -46,7 +46,8 @@ public class PlayerNetwork : NetworkBehaviour
 
         if(Input.GetKeyDown(KeyCode.T))
         {
-            TestServerRpc(new ServerRpcParams());
+            //TestServerRpc(new ServerRpcParams());
+            TestClientRpc();
             /*
             randomNumber.Value = new MyCustomData {
                 _int = 10,
@@ -70,6 +71,11 @@ public class PlayerNetwork : NetworkBehaviour
     [ServerRpc]
     private void TestServerRpc(ServerRpcParams serverRpcParams) {
         Debug.Log("TestSErverRpc" + OwnerClientId + "; " + serverRpcParams.Receive.SenderClientId); 
+    }
+
+    [ClientRpc]
+    void TestClientRpc() {
+        Debug.Log("testclient rpc");
     }
 
 }
